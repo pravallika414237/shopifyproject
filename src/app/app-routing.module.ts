094785741925c10d174d2afa13c9e04ba6942c34
pravallika414237 <pravallikasatya22@gmail.com>
2023-01-17 09:19:47 +0530

    Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { LoginComponent } from './component/login/login.component'
 import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 import { ViewProductComponent } from './component/view-product/view-product.component';
 import { DeliveryDetailsComponent } from './component/delivery-details/delivery-details.component';
+
 const routes: Routes = [
-  {path : '' , redirectTo:'products' , pathMatch: 'full'},
+  {path : '', redirectTo:'products' , pathMatch: 'full'},
   {path: 'products' ,component:ProductsComponent},
   {path: 'products/cart' ,component:CartComponent},
   {path:'products/viewproduct/:pid',component:ViewProductComponent},
   {path: 'products/login' ,component:LoginComponent},
-  {path: '**' ,component:PageNotFoundComponent},
-  {path: 'products/cart/delivery', component:DeliveryDetailsComponent}
+  {path:'delivery', component:DeliveryDetailsComponent},
+   {path: '**' ,component:PageNotFoundComponent}
   
 ];
 
--- src/app/component/delivery-details/delivery-details.component.ts
+++ src/app/component/delivery-details/delivery-details.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartserviceService } from 'src/app/services/cartservice.service';
 @Component({
   selector: 'app-delivery-details',
   templateUrl: './delivery-details.component.html',
   styleUrls: ['./delivery-details.component.css']
 })
 export class DeliveryDetailsComponent implements OnInit {
-
-  constructor() { }
+ 
+  public grandTotal !:number;
+ 
+  constructor(private cartService: CartserviceService) {
+    // this.reactiveabc=formboj.group({
+      
+    //   fname:new FormControl('',[Validators.required,Validators.pattern("[a-zA-Z]+$")]),
+    //   lname:new FormControl('',[Validators.required,Validators.minLength(3)]),
+    //   mail:new FormControl('',[Validators.required,Validators.email]),
+    //   phone:new FormControl('',[Validators.required,Validators.pattern("[0-9]{10}")]),
+    //   citizen:new FormControl('',[Validators.required]),
+    //   c1:new FormControl('',Validators.requiredTrue)
+    // })
+   }
 
   ngOnInit(): void {
+    this.cartService.getProducts().subscribe(res=>{
+      
+      this.grandTotal=this.cartService.getTotalPrice();
+    })
   }
-
+  
+  order(){
+alert("order placed successfully... you will shortly recieve a message.")
+  }
+  
 }
